3a6d8eb036bd37a9ef58ac4d739a538796f0dcd9
Harry <harryle.au@gmail.com>
2018-07-14 04:26:26 +1000

    fix deployment bugs

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use('/api/todos', todosRoute);
 app.use('/api/users', usersRoute);
 
 if(process.env.NODE_ENV === 'production') {
-  app.use(static('client/public'));
+  app.use(express.static('client/public'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
